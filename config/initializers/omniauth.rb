3e2419b1ffb9d1450ca828a9256fe1db35552152
Dmitry Sky <dmitry.sky@icloud.com>
2015-09-08 21:27:03 -0600

    fixed typo in redirect_uri in instagrams dev dashboard

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :developer unless Rails.env.production?
   provider :instagram, ENV["instagram_id"], ENV["instagram_secret"]
 end
