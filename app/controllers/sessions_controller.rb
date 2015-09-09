9ad16d6c628f78097b492c67c62c38eba169080a
Dmitry Sky <dmitry.sky@icloud.com>
2015-09-08 21:06:32 -0600

    provider is a developer unless in production

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     user = User.find_or_create_from_auth(oauth_data)
 
     if user
-      session[:user_id] = user.id
+      session[:user_id]  = user.id
       flash[:success]    = "Welcome"
       redirect_to root_path
     else
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :developer unless Rails.env.production?
   provider :instagram, ENV["instagram_id"], ENV["instagram_secret"]
 end
